3ade6a4303eff4a19158f00145b85ece55d6db53
Mohd Saad <shaadansari8081@gmail.com>
2025-07-31 14:41:26 +0530

    Update .gitignore to include .env and modify server port configuration

--- server/src/app.js
+++ server/src/app.js
@@ -34,7 +34,7 @@ app.use("/",marketRouter)
 connectDB()
   .then(() => {
     console.log("Connected to MongoDB successfully");
-    app.listen(3000, () => {
+    app.listen(process.env.PORT || 5000, () => {
       console.log("Server is running on port 3000");
     });
   })
